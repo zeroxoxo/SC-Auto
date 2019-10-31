58cd014953c8968963d6a126f3e758292d894176
zeroxoxo <32829385+zeroxoxo@users.noreply.github.com>
2019-10-31 22:13:16 +0700

    Refactor some ugly code, add doc strings.

--- SC_Auto.py
+++ SC_Auto.py
@@ -4,11 +4,10 @@ import soundcloud as sc
 import random
 
 
-token = 'insert_your_access_token'
+token = 'access_token'
 client_xeno = sc.Client(access_token=token)
 me = client_xeno.get('me')
 
-#list of comments
 comments = ['ayyyyyyyy', 'nice one :)', 'awesomeeee', 'incredible work dude', 'woah!!!', 'toooo fkn awesome',
             'luv it!', 'awesome!!', 'daaayum', ';D', 'dope one', 'amzng', 'aye', 'soo niiice', 'so good',
             'XDDDDDDDDD', 'dope af', 'nice one', 'XD', 'omfg', 'wow', 'awesomeeee', 'dope', 'incredible',
@@ -26,179 +25,163 @@ comments = ['ayyyyyyyy', 'nice one :)', 'awesomeeee', 'incredible work dude', 'w
             'aww yess', 'sic1', 'geeenius!!!', 'really vivid', 'paints a pic']
 
 
-read = open('reject.txt', mode='r')
-reject = read.read().split(sep=' ')
-read.close()
-for i in range(len(reject)-1):
-    reject[i] = int(reject[i])
+with open('reject.txt', mode='r') as read:
+    reject = list(map(int, read.read().split()))
+
+
 add = open('reject.txt', mode='a')
 
-def sc_iFollow():
-    out = []
+def sc_iFollow() -> list:
+    "Get the list of user ids, that I follow."
+
     i_follow = client_xeno.get('/me/followings/', linked_partitioning=1, limit=200, order='id')
-    for f in i_follow.collection:
-        out.append(f.id)
+    out = [f.id for f in i_follow.collection]
+
     while i_follow.next_href != None:
         i_follow = client_xeno.get(i_follow.next_href, linked_partitioning=1, limit=200, order='id')
-        for f in i_follow.collection:
-            out.append(f.id)
+        out += [f.id for f in i_follow.collection]
+
     return out
 
 
-def sc_myFollowers():
-    out = []
-    i_follow = client_xeno.get('/me/followers/', linked_partitioning=1, limit=200, order='id')
-    for f in i_follow.collection:
-        out.append(f.id)
-    while i_follow.next_href != None:
-        i_follow = client_xeno.get(i_follow.next_href, linked_partitioning=1, limit=200, order='id')
-        for f in i_follow.collection:
-            out.append(f.id)
+def sc_myFollowers() -> list:
+    "Get the list of user ids, that follow me."
+
+    my_follow = client_xeno.get('/me/followers/', linked_partitioning=1, limit=200, order='id')
+    out = [f.id for f in my_follow.collection]
+
+    while my_follow.next_href != None:
+        my_follow = client_xeno.get(my_follow.next_href, linked_partitioning=1, limit=200, order='id')
+        out += [f.id for f in my_follow.collection]
+
     return out
 
 
-def sc_iLike():
-    out = []
+def sc_iLike() -> list:
+    "Get the list of user ids, that I like."
     i_like = client_xeno.get('/me/favorites/', linked_partitioning=1, limit=200, order='id')
-    for f in i_like.collection:
-        out.append(f.id)
+    out = [f.id for f in i_like.collection]
     try:
         while i_like.next_href != None:
             i_like = client_xeno.get(i_like.next_href, linked_partitioning=1, limit=200, order='id')
-            for f in i_like.collection:
-                out.append(f.id)
+            out += [f.id for f in i_like.collection]
     except AttributeError:
         pass
     return out
 
 
-def sc_Like(id):
+def sc_Like(id: int):
+    "Add track in favorites, by track id."
     try:
         client_xeno.put('/me/favorites/' + str(id))
     except Exception as e:
-        print(str(e))
-    return
+        print(e)
 
 
-def sc_postComment(id):
+def sc_postComment(id: int):
+    "Post random comment on track, by track id."
     try:
-        x = client_xeno.get('/tracks/%d/' % id).duration
+        x = client_xeno.get('/tracks/{id}/'.format(id)).duration
         if int(me.id) != int(id):
-            client_xeno.post('/tracks/%d/comments' % id, comment={
-                'body': comments.pop(random.randint(0, len(comments))),
+            client_xeno.post('/tracks/{id}/comments'.format(id), comment={
+                'body': comments.pop(random.choice(comments)),
                 'timestamp': x/random.randint(2, 10)
             })
     except Exception as e:
         print(e)
-    return
 
 
-def sc_Follow(id):
+def sc_Follow(id: int):
+    "Follow user, by user id."
     try:
-        client_xeno.put('/me/followings/' + str(id))
+        client_xeno.put('/me/followings/{id}'.format(id))
     except Exception as e:
-        print(str(e))
-    return
+        print(e)
 
 
-def sc_Unfollow(id):
+def sc_Unfollow(id: int):
+    "Unfollow user, by user id."
     try:
-        client_xeno.delete('/me/followings/' + str(id))
+        client_xeno.delete('/me/followings/{id}'.format(id))
         add.write(str(id) + ' ')
     except Exception as e:
-        print(str(e))
-    return
+        print(e)
 
 
-def sc_FollowFollowers(id):
-    print('start sc_FollowFollowers')
-    new_follow = client_xeno.get('/users/%d/followers/' % id, limit=200, linked_partitioning=1)
-    i_f = sc_iFollow()
-    count = 40 + random.randint(0,10)
-    x = 0
-    j = []
-    for i in new_follow.collection:
-        if i.id not in i_f and x < count:
-            if i.id not in j and i.followers_count >= 120:
-                if i.id not in reject:
-                    try:
-                        t = client_xeno.get('/users/%d/tracks/' % i.id)[0].id
-                        sc_Like(t)
-                        sc_postComment(t)
-                    except:
-                        pass
-                    print('Followed ' + i.username)
-                    j.append(i.id)
-                    sc_Follow(i.id)
-                    x += 1
-        elif x >= count:
-            return
-    while new_follow.next_href != None:
-        new_follow = client_xeno.get(new_follow.next_href, limit=200, linked_partitioning=1)
-        for i in new_follow.collection:
-            if i.id not in i_f and x < count:
-                if i.id not in j and i.followers_count >= 120:
-                    if i.id not in reject:
-                        try:
-                            t = client_xeno.get('/users/%d/tracks/' % i.id)[0].id
-                            sc_Like(t)
-                            sc_postComment(t)
-                        except:
-                            pass
-                        print('Followed ' + i.username)
-                        j.append(i.id)
-                        sc_Follow(i.id)
-                        x += 1
+def sc_followFollowers(id: int):
+    "Follow users that follow some other person if they have enough followings as well, until count is not reached."
+    print('Following...')
+    i_f, count, reject_count, x, j = sc_iFollow(), 40 + random.randint(0,10), 120, 0, []
+    new_follow = client_xeno.get(f'/users/{id}/followers/', limit=1, linked_partitioning=1)
+    next_href = new_follow.next_href
+    new_follow_next = client_xeno.get(next_href, limit=200, linked_partitioning=1)
+    while new_follow_next.next_href != None:
+        next_href = new_follow_next.next_href
+        for i in new_follow_next.collection:
+            if (((i.id not in i_f) and (x < count)) and ((i.id not in j) and (i.followers_count >= reject_count))) and (i.id not in reject):
+                try:
+                    t = client_xeno.get(f'/users/{i.id}/tracks/')[0].id
+                    sc_Like(t)
+                    sc_postComment(t)
+                except:
+                    pass
+                print('Followed ' + i.username)
+                j.append(i.id)
+                sc_Follow(i.id)
+                x += 1
             elif x >= count:
+                print('Following done.')
                 return
+        del new_follow_next
+        new_follow_next = client_xeno.get(next_href, limit=200, linked_partitioning=1)
 
 
 def sc_cleanFollowers():
-    print('start sc_cleanFollowers')
-    count = 0
+    "Clear space in followings."
+    print('Unfollowing...')
     followers = sc_myFollowers()
     followings = sc_iFollow()
-    for i in followings:
-        print('Checking ' + client_xeno.get('/users/' + str(i)).username)
-        if (i not in followers and client_xeno.get('/users/' + str(i)).followers_count < 9000) or (client_xeno.get('/users/' + str(i)).followers_count < 120):
-            if count < 50:
+    low_edge, high_edge, unfollow_rate = 130, 9000, 50
+    for n, i in enumerate(followings):
+        user = client_xeno.get(f'/users/{i}')
+        print('Checking ' + user.username)
+        if (i not in followers and user.followers_count < high_edge) or (user.followers_count < low_edge):
+            if n < unfollow_rate:
                 sc_Unfollow(i)
-                print('Unfollowed ' + client_xeno.get('/users/' + str(i)).username)
-                count+=1
+                print('Unfollowed ' + user.username)
             else:
-                print('50 done')
-                break
-    print('cleanFollowers done')
-    return
+                print('Unfollowing done.')
+                return
+    print('Adjust edges.')
 
 
+with open('f.txt', 'r') as flag:
+    f = flag.read(1)
 
-flag = open('f.txt', 'r')
-f = flag.read(1)
-flag.close()
 
-fw = open('f.txt', 'w+')
-
-if f == 'c':
-    sc_cleanFollowers()
+with open('f.txt', 'w+') as fw:
     mfc = client_xeno.get('me').followings_count
-    if mfc <= 1700:
-        fw.write('f sample text')
-        print('Done')
+    if f == 'c':
+        sc_cleanFollowers()
+
+        if mfc <= 1700:
+            fw.write('f sample text')
+            print('Done')
+        else:
+            fw.write('c sample text')
+            print('Done')
+    elif f == 'f':
+        sc_followFollowers(426079)
+
+        if mfc >= 1900:
+            fw.write('c sample text')
+            print('Done')
+        else:
+            fw.write('f sample text')
+            print('Done')
     else:
         fw.write('c sample text')
-        print('Done')
-elif f == 'f':
-    sc_FollowFollowers(251087)
-    mfc = client_xeno.get('me').followings_count
-    if mfc >= 1900:
-        fw.write('c sample text')
-        print('Done')
-    else:
-        fw.write('f sample text')
-        print('Done')
+        print('Something get wrong...')
 
-
-fw.close()
 
 add.close()
